994f750688282ae63839d4793f3cfe16f78a5d1a
DANG NGOC HIEU <hieudn@ominext.com>
2023-01-06 17:53:56 +0700

    coding

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.enableCors({
+    origin: 'http://127.0.0.1:5173',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type']
+  })
   await app.listen(5000)
 }
 bootstrap()
